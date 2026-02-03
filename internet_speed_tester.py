7b160bf4f3d07526e401481f2a3d231656246284
Thato M <thsekeec025@student.wethinkcode.co.za>
2026-02-03 22:24:54 +0200

    final test

--- internet_speed_tester.py
+++ internet_speed_tester.py
@@ -10,4 +10,14 @@ def check_internet_speed():
 
     speed_test.get_best_server()
     ping = speed_test.results.ping
+    return {
+        "download": round(download, 2),
+        "upload": round(upload, 2),
+        "ping": round(ping, 2)
+    }
 
+speed = check_internet_speed()
+
+print(f"Download: {speed['download']} Mbps")
+print(f"Upload: {speed['upload']} Mbps")
+print(f"Ping: {speed['ping']} ms" )
